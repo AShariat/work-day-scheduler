69558a89b200972b344f3de0ef251772345565a3
Amir Shariat <amirshariat1987@gmail.com>
2022-01-18 01:40:24 -0700

    created 9 working hours with jquery and bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,22 @@
 // this code block does two thing, it first shows the current date and time then refreshes the time every second so our time display line stays updated
-var m = moment().format('dddd MMMM Do YYYY, h:mm:ss a');
-$('#currentDay').text(m);
+var now = moment().format('dddd MMMM Do YYYY, h:mm:ss a');
+$('#currentDay').text(now);
 setInterval(function() {
-  var m = moment().format('dddd MMMM Do YYYY, h:mm:ss a');
-  $('#currentDay').text(m);
+  now = moment().format('dddd MMMM Do YYYY, h:mm:ss a');
+  $('#currentDay').text(now);
 }, 1000);
+// this code block creates 9 rows for 9 working hours
+for (var i = 0; i < 9; i++) {
+  var workHour = $('<div></div>');
+  $(workHour).addClass('row time-block');
+  $('main').append(workHour);
+  var workHourTime = $('<div></div>');
+  $(workHourTime).addClass('col-1 hour').text(i+9);
+  $(workHour).append(workHourTime);
+  var workHourTask = $('<textarea></textarea>');
+  $(workHourTask).addClass('col-10 description');
+  $(workHour).append(workHourTask);
+  var workHourSave = $('<button></button>');
+  $(workHourSave).addClass('col-1 saveBtn').text("Save");
+  $(workHour).append(workHourSave);
+};
