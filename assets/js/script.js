3b54fb92f7a6b2c2f382429ca3890cf32c232abc
Amir Shariat <amirshariat1987@gmail.com>
2022-01-18 17:35:16 -0700

    added delete all function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,19 +9,28 @@ setInterval(function() {
 var timeNumber = moment().format('HH');
 setInterval(function() {
   var timeNumber = moment().format('HH');
-  console.log(timeNumber);
 }, 1000);
 // this code block creates 9 rows for 9 working hours
 for (var i = 0; i < 9; i++) {
   var workTime = i+9;
   var workHour = $('<div></div>');
   $(workHour).addClass('row time-block');
+  $(workHour).addClass("time-block-"+workTime);
   $('#schedule').append(workHour);
   var workHourTime = $('<div></div>');
-  $(workHourTime).addClass('col-1 hour').text(workTime + " am/pm");
+  if (workTime > 12) {
+    var workTimeAmPm = workTime-12 + " pm";
+  } else if (workTime == 12) {
+    workTimeAmPm = workTime + " pm";
+  } else {
+    workTimeAmPm = workTime + " am";
+  }
+  $(workHourTime).addClass('col-1 hour').text(workTimeAmPm);
+  $(workHourTime).addClass("hour-"+workTime);
   $(workHour).append(workHourTime);
-  var workHourTask = $('<textarea></textarea>');
+  var workHourTask = $('<input>');
   $(workHourTask).addClass('col-10 description');
+  $(workHourTask).addClass("task-"+workTime);
   // this code block compares the current time to the working hours and color code them accordingly
   if (workTime == timeNumber) {
     $(workHourTask).addClass('present');
@@ -33,5 +42,22 @@ for (var i = 0; i < 9; i++) {
   $(workHour).append(workHourTask);
   var workHourSave = $('<button></button>');
   $(workHourSave).addClass('col-1 saveBtn').text("Save");
+  $(workHourSave).addClass("save-"+workTime);
   $(workHour).append(workHourSave);
 };
+
+$('.save-9').click(function (e) { 
+  e.preventDefault();
+  var task = document.querySelector('.task-9').value;
+  if (task == "") {
+    console.log("Empty");
+  } else {
+    console.log(task);
+  }  
+});
+
+$('#delete-all').click(function(e) {
+  e.preventDefault();
+  localStorage.clear();
+  window.location.reload();
+});
